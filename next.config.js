581899b627b96d4312f14333b41ee325329c40fc
0xbatutut <39509105+0xbatutut@users.noreply.github.com>
2018-11-01 23:38:57 +0700

    Minify CSS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withSass = require('@zeit/next-sass');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = withSass();
 
 module.exports = withSass({
@@ -7,6 +9,8 @@ module.exports = withSass({
       test: /\.(raw)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       use: 'raw-loader',
     });
+    config.optimization.minimize = true;
+    config.optimization.minimizer = [new OptimizeCSSAssetsPlugin({})];
     return config;
   }
 });
